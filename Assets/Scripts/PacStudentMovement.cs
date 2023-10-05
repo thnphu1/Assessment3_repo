701b2df5a4086504df9b610214069ab0b26b9830
Thien Phu Tran <Thien.P.Tran-1@student.uts.edu.au>
2023-10-05 23:05:22 +1100

    pacStudentMovement

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -4,58 +4,18 @@ using UnityEngine;
 
 public class PacStudentMovement : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 5f; // Adjust the movement speed as needed.
 
-    private Vector2 currentDirection = Vector2.zero; // Pac-Man starts with no movement
-    private bool isDead = false;
-
-    private void Update()
+    void Update()
     {
-        if (!isDead)
-        {
-            HandleInput();
-            Move();
-        }
-    }
+        // Get input from the player.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-    private void HandleInput()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            // Rotate Pac-Man 90 degrees to the left and set the direction
-            transform.rotation = Quaternion.Euler(0, 0, 90);
-            currentDirection = Vector2.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            // Rotate Pac-Man 90 degrees to the right and set the direction
-            transform.rotation = Quaternion.Euler(0, 0, -90);
-            currentDirection = Vector2.right;
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            // Rotate Pac-Man upwards and set the direction
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            currentDirection = Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Rotate Pac-Man 180 degrees downwards and set the direction
-            transform.rotation = Quaternion.Euler(0, 0, 180);
-            currentDirection = Vector2.down;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Pac-Man "dies" (disappears) when you press the spacebar
-            isDead = true;
-            gameObject.SetActive(false); // Hide Pac-Man
-        }
-    }
+        // Calculate the movement vector based on input.
+        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * moveSpeed;
 
-    private void Move()
-    {
-        // Move Pac-Man in the current direction
-        Vector2 newPosition = (Vector2)transform.position + currentDirection * moveSpeed * Time.deltaTime;
-        transform.position = newPosition;
+        // Move the GameObject.
+        transform.Translate(movement * Time.deltaTime);
     }
 }
